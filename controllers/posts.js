4ba0daeb6dcba24ea1a26588bae2e9eca8a5dc87
arkawWebDev <arkar84848484@gmail.com>
2023-07-17 02:07:21 +0630

    More With Sequelize

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,7 +22,7 @@ exports.renderCreatePage = (req, res) => {
 };
 
 exports.getPosts = (req, res) => {
-  Post.findAll()
+  Post.findAll({ order: [["createdAt", "desc"]] })
     .then((posts) => {
       res.render("home", { title: "Home Page", postsArr: posts });
     })
@@ -41,4 +41,47 @@ exports.getPost = (req, res) => {
       res.render("details", { title: "Post Details Page", post });
     })
     .catch((err) => console.log(err));
+};
+
+exports.deletePost = (req, res) => {
+  const postId = req.params.postId;
+  Post.findByPk(postId)
+    .then((post) => {
+      if (!post) {
+        res.redirect("/");
+      }
+      return post.destroy();
+    })
+    .then((result) => {
+      console.log("Post Deleted!!");
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getOldPost = (req, res) => {
+  const postId = req.params.postId;
+  Post.findByPk(postId)
+    .then((post) => {
+      res.render("editPost", { title: `${post.title}`, post });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.updatePost = (req, res) => {
+  const { title, description, photo, postId } = req.body;
+  Post.findByPk(postId)
+    .then((post) => {
+      (post.title = title),
+        (post.description = description),
+        (post.imgUrl = photo);
+      return post.save();
+    })
+    .then((result) => {
+      console.log(`Post id => ${postId} is updated successfully.`);
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,20 @@ const express = require("express");
 const path = require("path");
 
 const router = express.Router();
-const postController = require("../controllers/posts")
+const postController = require("../controllers/posts");
 
 // /admin/create-post
 router.get("/create-post", postController.renderCreatePage);
 
 router.post("/", postController.createPost);
 
-module.exports = router
+// /admin/post/id
+router.post("/post/:postId", postController.deletePost);
+
+// admin/post-edit
+router.post("/post-edit", postController.updatePost);
+
+// admin/post-edit/id
+router.get("/post-edit/:postId", postController.getOldPost);
+
+module.exports = router;
